d0eee90abf86545b2430d95f65700c7c18e9da6b
Gabriel de Lacerda Rangel <gabriel.rangel@turismo.gov.br>
2019-08-23 15:13:42 -0300

    Making of the igraph objects and Plottings

--- script_redes.R
+++ script_redes.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(readxl)
-
+library(igraph)
 
 Ano11 <- read_excel("data/AnoaAno.xlsx", sheet = "2011")
 Ano11 <- select(Ano11, -'Ano')
@@ -84,7 +84,206 @@ write.csv(mx_2018, file = "Trip_18.csv")
 mx_2019 <- as.data.frame(mx_2019)
 write.csv(mx_2019, file = "Trip_19.csv")
 
-#-----------------------
+
+#------------- Making the Igraph Object --------
+
+#2018---
+
+Trip_18 <- read_excel("~/redes/data/Trip_18.xlsx")
+Trip_18 <- Trip_18[,-1]
+Trip_18 <- as.matrix(Trip_18)
+rownames(Trip_18) <- colnames(Trip_18)
+
+
+Itrip_18 <- simplify(graph_from_adjacency_matrix(Trip_18, mode = "Undirected"))
+#Itrip_18 <- delete.vertices(Itrip_18, degree(Itrip_18) == 0)
+head(Itrip_18)
+
+
+#2011---
+
+
+Trip_11 <- read_excel("~/redes/data/Trip_11.xlsx")
+Trip_11 <- Trip_11[,-1]
+Trip_11 <- as.matrix(Trip_11)
+rownames(Trip_11) <- colnames(Trip_11)
+
+
+Itrip_11 <- simplify(graph_from_adjacency_matrix(Trip_11, mode = "Undirected"))
+#Itrip_11 <- delete.vertices(Itrip_11, degree(Itrip_11) == 0)
+head(Itrip_11)
+
+
+#2012---
+
+
+Trip_12 <- read_excel("~/redes/data/Trip_12.xlsx")
+Trip_12 <- Trip_12[,-1]
+Trip_12 <- as.matrix(Trip_12)
+rownames(Trip_12) <- colnames(Trip_12)
+
+
+Itrip_12 <- simplify(graph_from_adjacency_matrix(Trip_12, mode = "Undirected"))
+#Itrip_12 <- delete.vertices(Itrip_12, degree(Itrip_12) == 0)
+head(Itrip_12)
+
+
+#2013---
+
+
+Trip_13 <- read_excel("~/redes/data/Trip_13.xlsx")
+Trip_13 <- Trip_13[,-1]
+Trip_13 <- as.matrix(Trip_13)
+rownames(Trip_13) <- colnames(Trip_13)
+
+
+Itrip_13 <- simplify(graph_from_adjacency_matrix(Trip_13, mode = "Undirected"))
+#Itrip_13 <- delete.vertices(Itrip_13, degree(Itrip_13) == 0)
+head(Itrip_13)
+
+
+
+#2014---
+
+
+Trip_14 <- read_excel("~/redes/data/Trip_14.xlsx")
+Trip_14 <- Trip_14[,-1]
+Trip_14 <- as.matrix(Trip_14)
+rownames(Trip_14) <- colnames(Trip_14)
+
+
+Itrip_14 <- simplify(graph_from_adjacency_matrix(Trip_14, mode = "Undirected"))
+#Itrip_14 <- delete.vertices(Itrip_14, degree(Itrip_14) == 0)
+head(Itrip_14)
+
+
+#2015---
+
+
+Trip_15 <- read_excel("~/redes/data/Trip_15.xlsx")
+Trip_15 <- Trip_15[,-1]
+Trip_15 <- as.matrix(Trip_15)
+rownames(Trip_15) <- colnames(Trip_15)
+
+
+Itrip_15 <- simplify(graph_from_adjacency_matrix(Trip_15, mode = "Undirected"))
+#Itrip_15 <- delete.vertices(Itrip_15, degree(Itrip_15) == 0)
+head(Itrip_15)
+
+
+
+#2016---
+
+
+Trip_16 <- read_excel("~/redes/data/Trip_16.xlsx")
+Trip_16 <- Trip_16[,-1]
+Trip_16 <- as.matrix(Trip_16)
+rownames(Trip_16) <- colnames(Trip_16)
+
+
+Itrip_16 <- simplify(graph_from_adjacency_matrix(Trip_16, mode = "Undirected"))
+#Itrip_16 <- delete.vertices(Itrip_16, degree(Itrip_16) == 0)
+head(Itrip_16)
+
+
+
+#2017---
+
+
+Trip_17 <- read_excel("~/redes/data/Trip_17.xlsx")
+Trip_17 <- Trip_17[,-1]
+Trip_17 <- as.matrix(Trip_17)
+rownames(Trip_17) <- colnames(Trip_17)
+
+
+Itrip_17 <- simplify(graph_from_adjacency_matrix(Trip_17, mode = "Undirected"))
+#Itrip_17 <- delete.vertices(Itrip_17, degree(Itrip_17) == 0)
+head(Itrip_17)
+
+
+#------- Plotting ------
+
+
+
+#2011---
+plot(Itrip_11,
+     vertex.size = 3,
+     vertex.color = "green",
+     layout=layout_with_fr,
+     #vertex.label = NA,
+     edge.color = adjustcolor("grey50", alpha = .3)
+)
+
+
+#2012---
+plot(Itrip_12,
+     vertex.size = 3,
+     vertex.color = "green",
+     layout=layout_nicely,
+     #vertex.label = NA,
+     edge.color = adjustcolor("grey50", alpha = .3)
+)
+
+
+
+#2013---
+plot(Itrip_13,
+     vertex.size = 3,
+     vertex.color = "green",
+     layout=layout_nicely,
+     vertex.label = NA,
+     edge.color = adjustcolor("grey50", alpha = .3)
+)
+
+
+#2014---
+plot(Itrip_14,
+     vertex.size = 3,
+     vertex.color = "green",
+     layout=layout_nicely,
+     vertex.label = NA,
+     edge.color = adjustcolor("grey50", alpha = .3)
+)
+
+
+#2015---
+plot(Itrip_15,
+     vertex.size = 3,
+     vertex.color = "green",
+     layout=layout_nicely,
+     vertex.label = NA,
+     edge.color = adjustcolor("grey50", alpha = .3)
+)
+
+
+#2016---
+plot(Itrip_16,
+     vertex.size = 3,
+     vertex.color = "green",
+     layout=layout_nicely,
+     vertex.label = NA,
+     edge.color = adjustcolor("grey50", alpha = .3)
+)
+
+
+#2017---
+plot(Itrip_17,
+     vertex.size = 3,
+     vertex.color = "green",
+     layout=layout_nicely,
+     vertex.label = NA,
+     edge.color = adjustcolor("grey50", alpha = .3)
+)
+
+
+#2018---
+plot(Itrip_18,
+     vertex.size = 3,
+     vertex.color = "green",
+     layout=layout_nicely,
+     vertex.label = NA,
+     edge.color = adjustcolor("grey50", alpha = .3)
+)
 
 
 
